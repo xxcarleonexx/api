0de750447952189c9f1eae1c160f94b504f2a287
carleone <xxcarleonexx@mail.ru>
2019-03-31 21:50:12 +0300

    WIP

--- api/user-task/read_paging.php
+++ api/user-task/read_paging.php
@@ -2,28 +2,23 @@
 
 use base\Db;
 use entity\UserTask;
+use utils\Utils;
 
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json; charset=UTF-8');
 
-// include database and object files
 include_once '../../base/core.php';
 include_once '../../base/Db.php';
-include_once '../utilities.php';
-include_once '../config/database.php';
-include_once '../objects/product.php';
+include_once '../../utils/Utils.php';
+include_once '../../entity/UserTask.php';
 
-// utilities
-$utilities = new Utilities();
+$utilities = new Utils();
 
-// instantiate database and product object
 $db = new Db();
 $connection = $db->getConnection();
 
-// initialize object
 $userTasks = new UserTask($connection);
 
-// query products
 $stmt = $userTasks->readPaging($from, $perPage);
 $num = $stmt->rowCount();
 
@@ -45,11 +40,11 @@ if ($num > 0) {
     $totalRows = $userTasks->count();
     $page_url = "{$homeUrl}userTask/read_paging.php?";
     $paging = $utilities->getPaging($page, $totalRows, $perPage, $page_url);
-    $products_arr["paging"] = $paging;
+    $userTasks['paging'] = $paging;
 
     http_response_code(200);
     echo json_encode($userTasks);
 } else {
     http_response_code(404);
-    echo json_encode(["message" => "No products found."]);
+    echo json_encode(['message' => 'No tasks found.']);
 }
